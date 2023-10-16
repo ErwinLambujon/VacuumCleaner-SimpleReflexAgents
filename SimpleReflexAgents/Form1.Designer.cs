eeda20a1410693a329e9fe38822b3b215e62a737
ErwinLambujon <144013045+ErwinLambujon@users.noreply.github.com>
2023-10-16 15:37:35 +0800

    Add files via upload
    Here's the complete source code of the Simple Reflex Agents using Vacuum Cleaner

---
+++ SimpleReflexAgents/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SimpleReflexAgents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(124, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.WaitOnLoad = true;
+            // 
+            // moveTimer
+            // 
+            this.moveTimer.Enabled = true;
+            this.moveTimer.Interval = 1000;
+            this.moveTimer.Tick += new System.EventHandler(this.MoveVacuum);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(178, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(612, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(178, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(612, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(125, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 31);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 462);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SimpleReflexAgents/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleReflexAgents
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random(Guid.NewGuid().GetHashCode());
+        int currentQuadrant = 0;
+        int targetX, targetY;
+        int step = 5;
+
+        List<string> labelOfStates = new List<string> { "Clean", "Clean", "Dirty", "Dirty" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            moveTimer = new Timer();
+            moveTimer.Tick += MoveVacuum;
+            moveTimer.Interval = 5000;
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void RandomQuadrant()
+        {
+            currentQuadrant = random.Next(1, 5);
+
+            int Xmin, Ymin, Xmax, Ymax;
+
+            switch (currentQuadrant)
+            {
+                case 1:
+                    Xmin = 0;
+                    Ymin = 0;
+                    Xmax = this.ClientSize.Width / 2;
+                    Ymax = this.ClientSize.Height / 2;
+                    break;
+                case 2:
+                    Xmin = this.ClientSize.Width / 2;
+                    Ymin = 0;
+                    Xmax = this.ClientSize.Width;
+                    Ymax = this.ClientSize.Height / 2;
+                    break;
+                case 3:
+                    Xmin = 0;
+                    Ymin = this.ClientSize.Height / 2;
+                    Xmax = this.ClientSize.Width / 2;
+                    Ymax = this.ClientSize.Height;
+                    break;
+                case 4:
+                    Xmin = this.ClientSize.Width / 2;
+                    Ymin = this.ClientSize.Height / 2;
+                    Xmax = this.ClientSize.Width;
+                    Ymax = this.ClientSize.Height;
+                    break;
+                default:
+                    Xmin = 0;
+                    Ymin = 0;
+                    Xmax = this.ClientSize.Width;
+                    Ymax = this.ClientSize.Height;
+                    break;
+            }
+
+            targetX = (Xmin + Xmax) / 2 - pictureBox2.Width / 2;
+            targetY = (Ymin + Ymax) / 2 - pictureBox2.Height / 2;
+
+            pictureBox2.Location = new Point(targetX, targetY);
+
+            labelOfStates[currentQuadrant - 1] = (random.Next(0, 2) == 0) ? "Clean" : "Dirty";
+
+            label1.Text = labelOfStates[0];
+            label2.Text = labelOfStates[1];
+            label3.Text = labelOfStates[2];
+            label4.Text = labelOfStates[3];
+        }
+
+
+
+
+        private void MoveVacuum(object sender, EventArgs e)
+        {
+
+            if (moveTimer.Enabled)
+            {
+                pictureBox2.Location = new Point(targetX, targetY);
+                LabelIntersection();
+                RandomTarget();
+                int x = targetX - pictureBox2.Location.X;
+                int y = targetY - pictureBox2.Location.Y;
+                int boundary_margin = 20;
+
+                if (pictureBox2.Left <= 0 || pictureBox2.Right >= this.ClientSize.Width - boundary_margin)
+                {
+                    x = 0;
+                }
+
+                if (pictureBox2.Top <= 0 || pictureBox2.Bottom >= this.ClientSize.Height - boundary_margin)
+                {
+                    y = 0;
+                }
+
+
+                if (Math.Abs(x) < step && Math.Abs(y) < step)
+                {
+                    pictureBox2.Location = new Point(targetX, targetY);
+                    RandomTarget();
+                }
+                else
+                {
+                    int newX = pictureBox2.Location.X + Math.Sign(x) * step;
+                    int newY = pictureBox2.Location.Y + Math.Sign(y) * step;
+
+                    newX = Math.Max(0, Math.Min(this.ClientSize.Width - pictureBox2.Width - boundary_margin, newX));
+                    newY = Math.Max(0, Math.Min(this.ClientSize.Height - pictureBox2.Height - boundary_margin, newY));
+
+                    pictureBox2.Location = new Point(newX, newY);
+                }
+
+                Console.WriteLine(targetX); // Debugging statement rani ha..
+                Console.WriteLine(targetY); // Debugging Statement rani ha..
+            }
+        }
+
+        private void LabelIntersection()
+        {
+            if (pictureBox2.Bounds.IntersectsWith(label1.Bounds))
+            {
+                if (labelOfStates[0] == "Dirty")
+                {
+                    labelOfStates[0] = "Clean";
+                    label1.Text = labelOfStates[0];
+                }
+                // Console.WriteLine("Intersect"); Debugging statement rani brad mana jud ko hayz....
+            }
+
+
+            if (pictureBox2.Bounds.IntersectsWith(label2.Bounds))
+            {
+                if (labelOfStates[1] == "Dirty")
+                {
+                    labelOfStates[1] = "Clean";
+                    label2.Text = labelOfStates[1];
+                }
+                // Console.WriteLine("Intersect"); Debugging statement rani brad mana jud ko hayz....
+            }
+
+            if (pictureBox2.Bounds.IntersectsWith(label3.Bounds))
+            {
+                if (labelOfStates[2] == "Dirty")
+                {
+                    labelOfStates[2] = "Clean";
+                    label3.Text = labelOfStates[2];
+                }
+                // Console.WriteLine("Intersect"); Debugging statement rani brad mana jud ko hayz....
+            }
+            if (pictureBox2.Bounds.IntersectsWith(label4.Bounds))
+            {
+                if (labelOfStates[3] == "Dirty")
+                {
+                    labelOfStates[3] = "Clean";
+                    label4.Text = labelOfStates[3];
+                }
+                // Console.WriteLine("Intersect"); Debugging statement rani brad mana jud ko hayz....
+            }
+        }
+
+        private void RandomTarget()
+        {
+            int centerX = this.ClientSize.Width / 2;
+            int centerY = this.ClientSize.Height / 2;
+
+            int newQuadrant;
+
+            do
+            {
+                newQuadrant = random.Next(1, 5);
+            } while (newQuadrant == currentQuadrant || IsDiagonal(currentQuadrant, newQuadrant));
+
+            currentQuadrant = newQuadrant;
+
+            switch (currentQuadrant)
+            {
+                case 1:
+                    targetX = centerX / 2 - pictureBox2.Width / 2;
+                    targetY = centerY / 2 - pictureBox2.Height / 2;
+                    break;
+                case 2:
+                    targetX = centerX + centerX / 2 - pictureBox2.Width / 2;
+                    targetY = centerY / 2 - pictureBox2.Height / 2;
+                    break;
+                case 3:
+                    targetX = centerX / 2 - pictureBox2.Width / 2;
+                    targetY = centerY + centerY / 2 - pictureBox2.Height / 2;
+                    break;
+                case 4:
+                    targetX = centerX + centerX / 2 - pictureBox2.Width / 2;
+                    targetY = centerY + centerY / 2 - pictureBox2.Height / 2;
+                    break;
+            }
+        }
+
+
+
+        private bool IsDiagonal(int currentQuadrant, int newQuadrant)
+        {
+            if ((currentQuadrant == 1 && newQuadrant == 4) || (currentQuadrant == 4 && newQuadrant == 1) ||
+                (currentQuadrant == 2 && newQuadrant == 3) || (currentQuadrant == 3 && newQuadrant == 2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        private bool QuadrantsAllowed(int currentQuadrant, int newQuadrant)
+        {
+
+            switch (currentQuadrant)
+            {
+                case 1:
+                    return newQuadrant == 2;
+                case 2:
+                    return newQuadrant == 1 || newQuadrant == 3;
+                case 3:
+                    return newQuadrant == 2 || newQuadrant == 4;
+                case 4:
+                    return newQuadrant == 3;
+                default:
+                    return false;
+            }
+        }
+
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.Black, 5);
+            int centerX = this.ClientSize.Width / 2;
+            int centerY = this.ClientSize.Height / 2;
+
+            graphics.DrawLine(pen, 0, centerY, this.ClientSize.Width, centerY);
+            graphics.DrawLine(pen, centerX, 0, centerX, this.ClientSize.Width);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moveTimer.Stop();
+            // Console.WriteLine("button click"); Debugger again
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveTimer.Start();
+            // Console.WriteLine("button click"); Debugger again
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < labelOfStates.Count; i++)
+            {
+                labelOfStates[i] = (random.Next(0, 2) == 0) ? "Clean" : "Dirty";
+            }
+
+            label1.Text = labelOfStates[0];
+            label2.Text = labelOfStates[1];
+            label3.Text = labelOfStates[2];
+            label4.Text = labelOfStates[3];
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RandomQuadrant();
+        }
+    }
+}
